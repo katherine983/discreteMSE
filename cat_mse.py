7709fe400d10ac33cdf5e9c48042e07ecaa03578
katherine983 <53629686+katherine983@users.noreply.github.com>
2022-01-19 15:52:17 -0800

    create entropy.py
    created separate file to house the discrete-valued versions of sampen and apen. these versions removed the separate similarity_distance() function dependency and removed all of the functionality to incorporate a transition probability matrix to weight the similarity distances.

--- cat_mse.py
+++ cat_mse.py
@@ -16,36 +16,36 @@ import pandas as pd
 def sim_distance(xmi, m, xixjprob_df=None):
     """
     Calculates the number of vectors in xmi whose max elementwise similarity distance from the template vector, xi, is <=r.
-    
+
     If xixjprob_df provided:
-        The similarity distance between two vector elements is the reverse Kronecker delta 
+        The similarity distance between two vector elements is the reverse Kronecker delta
         times the complement of the conditional probability of the elementwise pair.
             Reverse Kronecker delta, d(x,y):
                 if x==y, then d(x,y) = 0;
                 if x!=y, then d(x,y) = 1
-            The values of the delta function are then multiplied by the complement of the conditional probability of the comparison vector element, 
+            The values of the delta function are then multiplied by the complement of the conditional probability of the comparison vector element,
             xj(l), given the template vector element, xi(k), based on a 1st order Markov model estimated on the full length of the original data sequence.
-            This ensures that elementwise pairs which are unlikely to occur together are weighted to have a larger sim_distance 
-            and pairs which are likely to occur together are weighted to have a smaller sim_distance. 
-            
+            This ensures that elementwise pairs which are unlikely to occur together are weighted to have a larger sim_distance
+            and pairs which are likely to occur together are weighted to have a smaller sim_distance.
+
             .max(axis=2) takes the max sim_distance for each xi-xj comparison array and relates to the degree of mismatch between the template vector and the xmi vectors.
-            
-        The final sum produces the number of vectors in xmi whose max sim_distance from xi is less than r. 
-    
+
+        The final sum produces the number of vectors in xmi whose max sim_distance from xi is less than r.
+
     Otherwise:
-         The similarity distance between two vector elements is the reverse Kronecker delta 
+         The similarity distance between two vector elements is the reverse Kronecker delta
         times the complement of the conditional probability of the elementwise pair.
             Reverse Kronecker delta, d(x,y):
                 if x==y, then d(x,y) = 0;
                 if x!=y, then d(x,y) = 1
-            
-    
+
+
     Parameters
     ----------
-    xmi : LIST CONTAINING ALL VECTORS OF LENGTH m DERIVED FROM THE DATA SEQUENCE 
+    xmi : LIST CONTAINING ALL VECTORS OF LENGTH m DERIVED FROM THE DATA SEQUENCE
     xi_xj_prob : DICT OF NORMALIZED FREQUENCIES OF 2-TUPLE VECTORS FROM ORIGINAL DATA SEQUENCE
     m : LENGTH OF VECTORS IN xmi
-    
+
 
     Returns
     -------
@@ -53,66 +53,66 @@ def sim_distance(xmi, m, xixjprob_df=None):
 
     """
     if xixjprob_df:
-        
+
         #z is the number of vectors in xmi
         z = len(xmi)
-        
+
         #create 3d array of xmi vectors where each xi in xmi is its own 1xm subarray
         xi_matrix = np.stack([xmi], axis=2).reshape((z,1,m))
-        
-        #dif is a 3D array containing the pairwise reverse kronecker delta between xi and xmi for all xi. 
+
+        #dif is a 3D array containing the pairwise reverse kronecker delta between xi and xmi for all xi.
         dif = np.invert(xi_matrix==xmi).astype(int)
-        
+
         #swap axes so that z by z arrays in the right orientation are made from broadcasting the arrays
-        xi_mat_vert = np.swapaxes(xi_matrix, 1,2) 
+        xi_mat_vert = np.swapaxes(xi_matrix, 1,2)
         xmi_ar_vert = np.swapaxes(xmi, 0,1)
-        
+
         xmi_bc = np.broadcast_arrays(xi_mat_vert, xmi_ar_vert)
-    
+
         #create a vertical (z,) array of all xi(k) in order of (xi-xj) comparisons
         xik_arrlist = np.split(xmi_bc[1], z, axis=2)
         xik_arr = np.vstack(xik_arrlist)
-        
+
         #create vertical array of all xj(l) in order of (xi-xj) comparisons
         xjl_arrlist = np.split(xmi_bc[0], z, axis=2)
         xjl_arr = np.vstack(xjl_arrlist)
-        
+
         #pair each xi(k) and xi(l) together in sequence to produce (z,2) array
         x_pairs = np.concatenate((xik_arr, xjl_arr), axis=2).reshape(z**2*m, 2)
         x_pairs_df = pd.DataFrame(x_pairs, columns=['xi_k', 'xj_l'])
-        
+
         #merge the x_pairs dataframe with the pairwise conditional probabilities dataframe so that each xi(k),xj(l) pair is matched with its associated probability
         p_x_pairs_df = x_pairs_df.merge(xixjprob_df, left_on=['xi_k','xj_l'], right_on=['xi_k','xj_l'], how='left')
-    
+
         #get shape of difference matrix
         nd=dif.shape
-        
+
         #Transform series of P(xj(l)|xi(k)) into numpy array with same shape as "dif"
         p_x_pairs = p_x_pairs_df['trans_p'].to_numpy()
         p_x_pairs = p_x_pairs.reshape(nd)
-        
-        #take the complement of p_x_pairs so that xixj transitions with low probability have a greater distance and 
+
+        #take the complement of p_x_pairs so that xixj transitions with low probability have a greater distance and
         #transitions with high probability have a lesser distance
         p_x_pairs = 1-p_x_pairs
-        
+
         #get the max normalized similarity distance of all xi from all xmi
-        #sim_dist = np.multiply(dif, p_x_pairs).max(axis=2) 
+        #sim_dist = np.multiply(dif, p_x_pairs).max(axis=2)
         #return np.sum(sim_dist <= r)
-        
+
         #multiple dif and p_x_pairs to get the weighted similarity distance for each xi,xj pair.
         #sim_dist is a (z,1,m) matrix
         sim_dist= np.multiply(dif, p_x_pairs).max(axis=2)
         return sim_dist
     else:
-        #z is the number of vectors in xmi
+        #z is the number of m-vectors in xmi
         z = len(xmi)
-        
+
         #create 3d array of xmi vectors where each xi in xmi is its own 1xm subarray
         xi_matrix = np.stack([xmi], axis=2).reshape((z,1,m))
-        
-        #dif is a 3D array containing the pairwise reverse kronecker delta between xi and xmi for all xi. 
+
+        #dif is a 3D array containing the pairwise inverse kronecker delta between xi and xmi for all xi.
         dif = np.invert(xi_matrix==xmi).astype(int)
-        
+
         #get the similarity difference as the number of xi(k),xj(l) pairs that do not match for each xi,xj pair
         sim_dist = dif.sum(axis=2)
         return sim_dist
@@ -124,14 +124,14 @@ def transition_prob(data):
 
     Parameters
     ----------
-    data : INPUT DATA SEQUENCE 
+    data : INPUT DATA SEQUENCE
 
     Returns
     -------
     xixjprob_df : pandas dataframe of xi(k),xj(l) probabilities; column[0]= xi_k value, column[1]= xj_l value, column[2]= P(xj(l)|xi(k)).
 
     """
-    
+
     xi_xj = list(windowed(data, 2))
     xi, xn = np.unique(data[:-1], return_counts=True)
     xi_counts = dict(zip(xi, xn))
@@ -140,20 +140,20 @@ def transition_prob(data):
     #print(xi_xj_counts)
     for i in xi_xj:
         xi_xj_counts[i] = xi_xj_counts.get(i, 0) +1
-        
-    #Dict of the normalized frequency ie proportion of each 2-tuple vector 
+
+    #Dict of the normalized frequency ie proportion of each 2-tuple vector
     xi_xj_prob = {}
     for k, v in xi_xj_counts.items():
         x_sum = xi_counts[k[0]]
         xi_xj_prob[k] = v/x_sum
-    
+
     #turn xi_xj_prob dictionary into a list of tuples
     xi_xj_probtups = list(sliced(list(collapse(xi_xj_prob.items())), 3))
     #print(xi_xj_probtups)
-    
+
     #create DataFrame of xi_xj probabilities from xi_xj_probtups
     xixjprob_df = pd.DataFrame(xi_xj_probtups, columns=['xi_k', 'xj_l', 'trans_p'])
-    
+
     return xixjprob_df
 
 def vector_matches(data, m, r, xixjprob_df=None, sampen=False, apen=False):
@@ -169,10 +169,10 @@ def vector_matches(data, m, r, xixjprob_df=None, sampen=False, apen=False):
     Returns
     -------
     matches
-    
+
     matches : integer representing the number of matching vectors length(m) within a distance r contained within the full sequence
 
-    """  
+    """
     xmi = np.array([data[i:i+m] for i in range(len(data)-m+1)])
     if xixjprob_df:
         sim_dist=sim_distance(xmi, m, xixjprob_df=xixjprob_df)
@@ -182,46 +182,47 @@ def vector_matches(data, m, r, xixjprob_df=None, sampen=False, apen=False):
         matches = np.sum(sim_dist<=r) - len(xmi)
     elif apen:
         matches= np.sum(sim_dist<=r, axis=1)
-    return matches    
-    
+    return matches
+
 def apen(data, m, r, sim_dist=None):
     A = vector_matches(data[:-1], m, r, apen=True)
     k=m+1
     B = vector_matches(data, k, r, apen=True)
-    
-    
-def sampen(data, m, r, sim_dist=None, xixjprob_df=None): 
+
+
+
+def sampen(data, m, r, sim_dist=None, xixjprob_df=None):
     """
     This is a function to measure the sample entropy of a given string.
-    
+
     Parameters
     ----------
-    data : RAW DATA SEQUENCE 
-    
-    m : THE LENGTH OF THE TEMPLATE SEQUENCE 
-    
+    data : RAW DATA SEQUENCE
+
+    m : THE LENGTH OF THE TEMPLATE SEQUENCE
+
     r : THE DISTANCE FROM THE TEMPLATE SEQUENCE THAT A POSSIBLE SEQUENCE IS CONSIDERED A MATCH
-    
+
     xi_xj_prob : DICT OF 1ST ORDER MARKOV TRANSITION PROBABILITIES
-    
+
     Returns
     -------
     sampen, pos, mat
-    
+
     sampen : floating point representing the Sample Entropy of the sequence
-    
+
     pos : integer representing number of possible matches, length m
-    
+
     mat : integer representing number of matches, length m+1
     """
 
     if xixjprob_df:
         #get number of possible matches, store in variable 'pos'
         #use data sequence with ultimate value removed so that number of m-length vectors equals the number of m+1-length vectors.
-        
-        pos = vector_matches(data[:-1], m, r, xixjprob_df, sampen=True) 
+
+        pos = vector_matches(data[:-1], m, r, xixjprob_df, sampen=True)
         #print(pos)
-    
+
         #get number of matches, store in variable 'mat'
         k = m+1
         mat = vector_matches(data, k, r, xixjprob_df, sampen=True)
@@ -229,10 +230,10 @@ def sampen(data, m, r, sim_dist=None, xixjprob_df=None):
     else:
         #get number of possible matches, store in variable 'pos'
         #use data sequence with ultimate value removed so that number of m-length vectors equals the number of m+1-length vectors.
-        
-        pos = vector_matches(data[:-1], m, r, sampen=True) 
+
+        pos = vector_matches(data[:-1], m, r, sampen=True)
         #print(pos)
-    
+
         #get number of matches, store in variable 'mat'
         k = m+1
         mat = vector_matches(data, k, r, sampen=True)
@@ -250,10 +251,10 @@ def sampen(data, m, r, sim_dist=None, xixjprob_df=None):
         sampen = -log(ratio)
         print(sampen, pos, mat)
         return sampen, pos, mat
-    
+
 def course_grain(raw_data, t):
     """
-    Creates a course-grained sequence from the input sequence from non-overlapping windows of size t. Uses the mode as the representative statistic of the t-windows. 
+    Creates a course-grained sequence from the input sequence from non-overlapping windows of size t. Uses the mode as the representative statistic of the t-windows.
 
     Parameters
     ----------
@@ -262,17 +263,17 @@ def course_grain(raw_data, t):
 
     Returns
     -------
-    Course-grained data sequence. 
+    Course-grained data sequence.
 
     """
     data = copy.deepcopy(raw_data.tolist())
     if t == 1:
         return raw_data
     else:
-        #create set of non-overlapping windows of size t. 
+        #create set of non-overlapping windows of size t.
         t_win = list(windowed(data, t, fillvalue=None, step=t))
-        
-    
+
+
         #initiate empty list of new course-grained sequence
         cg_seq = []
         for t in t_win:
@@ -280,8 +281,8 @@ def course_grain(raw_data, t):
                 continue
             else:
                 #create list of (mode, count) for each t_win where mode is the first symbol in the sequence with the highest frequency.
-                #Counter() used because in the case of multiple modes it returns the value that occurs first in the sequence, 
-                #rather than the value that is the least 
+                #Counter() used because in the case of multiple modes it returns the value that occurs first in the sequence,
+                #rather than the value that is the least
                 mode = Counter(t).most_common(1)
                 cg_seq.append(mode[0][0])
         return cg_seq
@@ -293,17 +294,17 @@ def multiscale(raw_data, m, r, t):
     Parameters
     ----------
     data : RAW DATA SEQUENCE OF INTEGERS REPRESENTING CODED CATEGORIES.
-    
+
     m : LENGTH OF THE TEMPLATE SEQUENCE.
-    
+
     r : THE DISTANCE FROM THE TEMPLATE SEQUENCE THAT A POSSIBLE SEQUENCE IS CONSIDERED A MATCH.
             r SHOULD BE AT LEAST EQUAL TO THE 1 MINUS THE CONDITIONAL PROBABILITY OF THE MOST LIKELY 1ST ORDER MARKOV PAIR
-    
+
     t : LIST OF WINDOW SIZES TO BE USED FOR THE COURSE-GRAINING PROCEDURE.
 
     Returns
     -------
-    Pandas dataframe of t values and corresponding sample entropy estimates. 
+    Pandas dataframe of t values and corresponding sample entropy estimates.
 
     """
     data = copy.deepcopy(raw_data)
@@ -332,5 +333,4 @@ if __name__ == "__main__":
     #data_minus_1 = data[:-1]
     #matches = vector_matches(data_minus_1, m, r)
     #print("Possible vector matches:", matches)
-    
-    
+
---
+++ entropy/entropy.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 18 20:12:55 2022
+
+@author: Wuestney
+
+Some variable naming is chosen to be in line with the notation used in the papers by Pincus (1991)
+and Richman & Moorman (2000).
+"""
+import numpy as np
+
+
+def vector_matches(data, m, r=0, enttype=None):
+    """
+    Calculates the number of sequences matching the template within the margin r.
+
+    Parameters
+    ----------
+    data : ITERABLE REPRESENTING THE FULL DATA SEQUENCE
+    m : TEMPLATE LENGTH
+    r : FILTER SIZE or MAX DISTANCE BETWEEN TWO VECTORS FOR THE VECTORS TO BE CONSIDERED MATCHING
+    enttype : NONE OR STRING
+        IF None WILL RETURN A SINGLE INTEGER SUM OVER ALL XI/XJ PAIRS. IF 'sampen'
+        WILL GIVE SUM OVER ALL XI/XJ PAIRS, SUBTRACTING len(data) TO REMOVE SELF
+        MATCHES. IF 'apen' WILL RETURN AN ARRAY OF THE NUMBER OF XI/XJ MATCHES
+        FOR EACH XI.
+
+    Returns
+    -------
+    matches
+
+    matches : integer representing the number of matching vectors length(m) within a distance r contained within the full sequence
+
+    """
+    xmi = np.array([data[i:i+m] for i in range(len(data)-m+1)])
+
+    #z is the number of m-vectors in xmi
+    z = len(xmi)
+
+    #create 3d array of xmi vectors where each xi in xmi is its own 1xm subarray
+    xi_matrix = np.stack([xmi], axis=2).reshape((z,1,m))
+
+    #dif is a 3D array containing the pairwise kronecker delta between xi and xmi for all xi.
+    dif = np.invert(xi_matrix==xmi).astype(int)
+    #dif.sum(axis=2) evaluates to 0 for xi that fully matched and >0 for xi that did not fully match
+    sim_dist = dif.sum(axis=2)
+
+    if enttype == 'sampen':
+        #subtract len(xmi) to remove self matches from the total
+        matches = np.sum(sim_dist==r) - len(xmi)
+    elif enttype == 'apen':
+        matches= np.sum(sim_dist==r, axis=1)
+    elif enttype == None:
+        matches = np.sum(sim_dist==r)
+    return matches
+
+def sampen(data, m):
+    """
+    This is a function to measure the sample entropy of a given string or
+    discrete-valued (categorical) time series. For this implementation, we follow
+    the same terminology and notation used by Richman & Moorman (2000). The
+    data sequence, X={u(1), u(2), u(i), ..., u(n-m)}, is used to form a sequence, xmi, of
+    m-length vectors where each xi in xmi is the vector of m data points from
+    u(i) to u(i+m-1). For each m-length vector, xm(i) for 1 <= i <= N-m, in X,
+    let Bi be the number of vectors, xm(j) for 1<=j<= N-m and j!= i, in X that match xm(i).
+    The xm(i) is called the template vector, and an xm(j) which matches
+    with xm(i) is called the template match. For each m+1-length vector, xm1(i) for
+    1 <= i <= N-m, let Ai be the number of vectors, xm1(j) for 1<=j<= N-m and j!= i,
+    in X that match xm1(i). Then let B be the sum of Bi over all i and A be the sum
+    of Ai over all i.
+
+    with the notable
+    exception that the parameter, r, is omitted. This is due to the fact that for
+    integer coded discrete-valued sequences, the tolerance value is set to r<1 and thus
+    the impact of r is suppressed because r < min(|x-y|, x != y, x and y state space values).
+    Therefore only template
+
+    Parameters
+    ----------
+    data : STRING OR LIST-LIKE ITERABLE OF INTEGERS
+        INTEGER CODED DISCRETE-VALUED DATA SEQUENCE.
+
+    m : INTEGER
+        THE LENGTH OF THE TEMPLATE SEQUENCE.
+
+    Returns
+    -------
+    sampen, B, A
+
+    sampen : floating point representing the Sample Entropy of the sequence
+
+    B : integer representing number of possible matches, length m
+
+    A : integer representing number of matches, length m+1
+    """
+
+    #get number of m-length matches, store in variable 'B'
+    #use data sequence with ultimate value removed so that number of m-length vectors equals the number of m+1-length vectors.
+
+    B = vector_matches(data[:-1], m, enttype='sampen')
+    #print(B)
+
+    #get number of matches, store in variable 'mat'
+    k = m+1
+    A = vector_matches(data, k, enttype='sampen')
+    #print(A)
+    if A == 0:
+        print("The data set is unique, there were no m+1-length matches.")
+        sampen = None
+        return sampen, B, A
+    if B == 0:
+        print("The data set is unique, there were no m-length matches.")
+        sampen = None
+        return sampen, B, A
+    else:
+        ratio = B/A
+        sampen = -np.log(ratio)
+        print(sampen, B, A)
+        return sampen, B, A
+
+def apen(data, m):
+    N = len(data)
+    Bi = vector_matches(data[:-1], m, enttype='apen')
+    k=m+1
+    Ai = vector_matches(data, k, enttype='apen')
+    print('N-m', N-m)
+    print('Bi', len(Bi), 'Ai', len(Ai))
+    print(Bi.shape == Ai.shape)
+    apen = np.sum(np.negative(np.log(Ai/Bi)))/(N-m)
+    return apen, Bi, Ai
