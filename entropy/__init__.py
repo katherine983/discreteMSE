1aed3b8e33573b4597b43c92595bcd62a7a57996
katherine983 <53629686+katherine983@users.noreply.github.com>
2022-01-19 15:58:25 -0800

    reorg entropy module

---
+++ entropy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 15:54:00 2022
+
+@author: Wuestney
+"""
+
--- entropy/entropy.py
+++ entropy/entropy.py
@@ -9,6 +9,36 @@ and Richman & Moorman (2000).
 """
 import numpy as np
 
+def discrete_renyi(n, alpha=2):
+    """
+    Given a frequency vector, this function calculates the discrete renyi entropy as 1/(1-alpha)*log2(sum(p**alpha))
+
+    Parameters
+    ----------
+    n : LIST LIKE OBJECT OF L-TUPLE FREQUENCY COUNTS
+    alpha : PARAMETER FOR THE RENYI OPERATION, DEFINES RENYI ORDER
+
+    Returns
+    -------
+    r, rmax, rmin : r is the discrete renyi(alpha) entropy of the sequence;
+            rmax and rmin are the max possible entropy for alphabet len(n) and the minimum entropy as alpha approaches infinity
+
+    """
+
+    p = np.array(n)/np.array(n).sum()
+
+    #define special case for Shannon's entropy alpha=1
+    if alpha==1:
+        p = np.where(p==0, 1, p)
+        r = -sum(p*np.log2(p))
+        print("Shannon's entropy is:", r)
+
+    else:
+        r= 1/(1-alpha)*np.log2(np.sum(p**alpha))
+    rmax = np.log2(len(n)) #the case of the uniform distribution or if alpha=0
+    rmin = -np.log2(np.max(p)) #the case as alpha -> infinity
+
+    return r, rmax, rmin
 
 def vector_matches(data, m, r=0, enttype=None):
     """
