864f76c131e5513b72e835aaceb989f579b98e34
katherine983 <53629686+katherine983@users.noreply.github.com>
2022-01-20 14:55:44 -0800

    add __init__.py file

--- discreteMSE/__init__.py
+++ discreteMSE/__init__.py
@@ -5,5 +5,5 @@ Created on Wed Jan 19 16:13:37 2022
 @author: Wuestney
 """
 
-import discreteMSE.entropy
-import discreteMSE.cat_mse
+import discreteMSE.discreteMSE.entropy
+import discreteMSE.discreteMSE.cat_mse
